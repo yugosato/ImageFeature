63a85f8be5e18ada0b434cb75b8be1e7a22980f6
yugosato <yg.physics72@gmail.com>
2017-06-29 17:49:07 +0900

    histogram-gabor

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 
 #include <iostream>
-#include <vector>
 #include <fstream>
 #include "color_histogram.h"
 #include "gabor_filtering.h"
@@ -30,11 +29,26 @@ void splitline(const std::string& s, const std::string& delim, std::vector<std::
 	}
 }
 
+void listcat(float* v, const float* a, const float* b, const int size_a, const int size_b)
+{
+	for (int i = 0; i < size_a; ++i)
+	{
+		v[i] = a[i];
+	}
+
+	for (int i = 0; i < size_b; ++i)
+	{
+		v[size_a + i] = b[i];
+	}
+}
+
 int main()
 {
 	std::string imageListtxt = "/home/yugo/Desktop/dataset/cfd-cropped/images_selected.txt";
-	std::ofstream ofs("cfd-gabor.tsv", std::ios::trunc);
-	int dim = 72; // histogram:64, gabor:72
+	std::ofstream ofs("cfd-histogram-gabor.tsv", std::ios::trunc);
+	const int hist_dim = 64;
+	const int gabor_dim = 72;
+	const int dim = hist_dim + gabor_dim;
 
 	std::ifstream ifs(imageListtxt.c_str());
 	if (ifs.fail())
@@ -42,16 +56,21 @@ int main()
 
 	std::string line;
 	std::vector<std::string> splited;
-//	Histogram hist;
+	Histogram hist;
 	Gabor gabor;
+	float result_hist[hist_dim];
+	float result_gabor[gabor_dim];
 	float result[dim];
 
 	while (getline(ifs, line))
 	{
 		splitline(line, " ", splited);
 		std::cout << splited[0] << std::endl;
-//		hist.calcHistogram(splited[0], result);
-		gabor.calcGaborFeature(splited[0], result);
+
+		hist.calcHistogram(splited[0], result_hist);
+		gabor.calcGaborFeature(splited[0], result_gabor);
+		listcat(result, result_hist, result_gabor, hist_dim, gabor_dim);
+
 		for (int i = 0; i < dim; ++i)
 		{
 			ofs << result[i];
